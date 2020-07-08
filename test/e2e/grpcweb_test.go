da20d351788008b0a27b8f174bdee68e5cbace3e
arianaw66 <ariana.weinstock@solo.io>
2020-07-08 19:56:38 -0400

    upgrade to v3 in projects/accesslogger

--- test/e2e/grpcweb_test.go
+++ test/e2e/grpcweb_test.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	envoy_data_accesslog_v2 "github.com/envoyproxy/go-control-plane/envoy/data/accesslog/v2"
-	envoyals "github.com/envoyproxy/go-control-plane/envoy/service/accesslog/v2"
+	envoy_data_accesslog_v3 "github.com/envoyproxy/go-control-plane/envoy/data/accesslog/v3"
+	envoyals "github.com/envoyproxy/go-control-plane/envoy/service/accesslog/v3"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
@@ -56,7 +56,7 @@ var _ = Describe("Grpc Web", func() {
 			Context("Grpc", func() {
 
 				var (
-					msgChan      <-chan *envoy_data_accesslog_v2.HTTPAccessLogEntry
+					msgChan      <-chan *envoy_data_accesslog_v3.HTTPAccessLogEntry
 					grpcUpstream *gloov1.Upstream
 				)
 
@@ -103,8 +103,8 @@ var _ = Describe("Grpc Web", func() {
 					toSend := &envoyals.StreamAccessLogsMessage{
 						LogEntries: &envoyals.StreamAccessLogsMessage_HttpLogs{
 							HttpLogs: &envoyals.StreamAccessLogsMessage_HTTPAccessLogEntries{
-								LogEntry: []*envoy_data_accesslog_v2.HTTPAccessLogEntry{{
-									CommonProperties: &envoy_data_accesslog_v2.AccessLogCommon{
+								LogEntry: []*envoy_data_accesslog_v3.HTTPAccessLogEntry{{
+									CommonProperties: &envoy_data_accesslog_v3.AccessLogCommon{
 										UpstreamCluster: "foo",
 									},
 								}},
@@ -148,7 +148,7 @@ var _ = Describe("Grpc Web", func() {
 						return nil
 					}, 10*time.Second, time.Second/10).Should(Not(HaveOccurred()))
 
-					var entry *envoy_data_accesslog_v2.HTTPAccessLogEntry
+					var entry *envoy_data_accesslog_v3.HTTPAccessLogEntry
 					Eventually(msgChan, time.Second).Should(Receive(&entry))
 					Expect(entry.CommonProperties.UpstreamCluster).To(Equal("foo"))
 				})
