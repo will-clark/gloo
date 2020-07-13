b959837f3cfa9775ced55c5dda964a2a9ed5d2bb
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-07-13 16:49:29 -0400

    fix script

--- projects/gloo/api/external/envoy/import.py
+++ projects/gloo/api/external/envoy/import.py
@@ -15,18 +15,18 @@ def get_imports(envoy_path, f):
         return set()
 
 
-def get_sorted_imports(f):
-    envoy_path = os.getenv("ENVOYPATH")
-    if not envoy_path:
-        raise Exception("please set ENVOYPATH to envoy's root folder")
+def get_sorted_imports(envoy_path, f):
     imports = list(get_imports(envoy_path, f))
-    folders = set()
     imports.sort()
     return imports
 
-def import_and_copy(f)
-    for i in get_sorted_imports(f):
-        path = "api/"+i
+def import_and_copy(f):
+    envoy_path = os.getenv("ENVOYPATH")
+    if not envoy_path:
+        raise Exception("please set ENVOYPATH to envoy's root folder")
+    for i in get_sorted_imports(envoy_path, f):
+        print("importing", i)
+        path = os.path.join(envoy_path,"api/",i)
         if os.path.exists(path):
             folder = os.path.dirname(i)
             option = 'option go_package = "github.com/solo-io/gloo/projects/gloo/pkg/api/external/'+folder+'";'
@@ -36,9 +36,9 @@ def import_and_copy(f)
                 print("Run these shell:")
                 f = lambda x: print(x)
             gopath = os.getenv("GOPATH")
-            if not gopath {
+            if not gopath:
                 gopath = os.getenv("HOME")+"/go"
-            }
+
             basedir = gopath+"/src/github.com/solo-io/gloo/projects/gloo/api/external/"
             f('mkdir -p ' + basedir + folder)
             dest = basedir +i
@@ -48,13 +48,13 @@ def import_and_copy(f)
             f("echo 'option (gogoproto.equal_all) = true;' >> " + dest)
 
 def main():
-    if len(sys.args) != 2:
+    if len(sys.argv) != 2:
         print("please run like so:")
-        print("    [COPY=1] [GOPATH=...] ENVOYPATH=... {} path-to-proto-in-envoy".format(sys.args[0]))
-        print("for example, this will copy route_components.proto and its dependencies to ~/go/src/github.com/solo-io/gloo/projects/gloo/api/external from ~/sources/envoy/api:")
-        print("    COPY=1 GOPATH=~/go ENVOYPATH=~/sources/envoy {} envoy/config/route/v3/route_components.proto".format(sys.args[0]))
+        print("    [COPY=1] [GOPATH=...] ENVOYPATH=... {} path-to-proto-in-envoy".format(sys.argv[0]))
+        print("for example, this will copy route_components.proto and its dependencies to $GOPATH/src/github.com/solo-io/gloo/projects/gloo/api/external from ~/sources/envoy/api:")
+        print("    COPY=1 GOPATH=~/go ENVOYPATH=~/sources/envoy {} envoy/config/route/v3/route_components.proto".format(sys.argv[0]))
         os.abort()
-    import_and_copy(sys.args[1])
+    import_and_copy(sys.argv[1])
 
 
 if __name__ == "__main__":
